3ec3cee7cfca857f6655da2786c23bbc3853f590
Aparajita Fishman <aparajita@aparajita.com>
2015-08-21 17:55:31 -0400

    Fixes/enhancements...
    - Inherit stack trace from global.Error.
    - Allow more options for location passed to add<type>.
    - Added a LF after the highlights.
    - Automatically lowercase the message when constructing the formatted output.
    - Split formatting and logging issues into separate functions and classes.

--- index.js
+++ index.js
@@ -47,27 +47,47 @@ function findLineEnd(source, pos)
     return source.length;
 }
 
-var Issue = exports.Issue = function(source, sourcePath, node, message, severity)
+var Issue = exports.Issue = function(source, sourcePath, location, message, severity)
 {
-    if (node.loc)
+    global.Error.call(this);
+
+    if (global.Error.captureStackTrace)
+        global.Error.captureStackTrace(this);
+
+    this.message = message || "";
+    this.name = "Issue";
+
+    /*
+        location could be:
+
+        - acorn.Node
+        - SyntaxError raised by acorn
+        - acorn.SourceLocation
+        - object with start property
+        - numeric start position
+    */
+    if (location.loc)
     {
         this.lineInfo = {
-            line: node.loc.line,
-            column: node.loc.column
+            line: location.loc.line,
+            column: location.loc.column
         };
     }
     else
-        this.lineInfo = acorn.getLineInfo(source, node.start);
+    {
+        if (typeof location === "number")
+            location = { start: location, end: location };
 
-    this.lineInfo.lineStart = node.start - this.lineInfo.column;
-    this.lineInfo.lineEnd = findLineEnd(source, node.end);
+        this.lineInfo = acorn.getLineInfo(source, location.start);
+    }
+
+    this.lineInfo.lineStart = location.start - this.lineInfo.column;
+    this.lineInfo.lineEnd = findLineEnd(source, location.end);
     this.lineInfo.sourceLength = source.length;
     this.source = trimRight(source.substring(this.lineInfo.lineStart, this.lineInfo.lineEnd));
     this.sourcePath = sourcePath;
     this.highlightedNodes = [];
-    this.message = message || "";
     this.severity = severity;
-    this.name = "Issue";
 };
 
 util.inherits(Issue, global.Error);
@@ -82,7 +102,7 @@ Issue.prototype.isError = function()
     return this.severity === "error";
 };
 
-Issue.prototype.getMessage = function(colorize)
+Issue.prototype.getFormattedMessage = function(colorize)
 {
     var enabled = chalk.enabled;
 
@@ -94,7 +114,7 @@ Issue.prototype.getMessage = function(colorize)
         messageTemplate = lodashTemplate(
             "${data.context} ${data.severity} ${data.message}\n" +
             "${data.source}\n" +
-            "${data.highlights}",
+            "${data.highlights}\n",
             { variable: "data" }
         );
     }
@@ -106,14 +126,14 @@ Issue.prototype.getMessage = function(colorize)
 
     for (var i = 0; i < this.highlightedNodes.length; i++)
     {
-        var node = this.highlightedNodes[i];
+        var location = this.highlightedNodes[i];
 
         // Make sure the highlight is in range of the source line
-        if (node.start >= this.lineInfo.lineStart &&
-            node.start < (this.lineInfo.lineStart + this.lineInfo.sourceLength))
+        if (location.start >= this.lineInfo.lineStart &&
+            location.start < (this.lineInfo.lineStart + this.lineInfo.sourceLength))
         {
-            var offset = node.start - this.lineInfo.lineStart,
-                length = node.end - node.start;
+            var offset = location.start - this.lineInfo.lineStart,
+                length = location.end - location.start;
 
             highlights = highlights.substring(0, offset) +
                          chalk[colorMap.highlight](repeat("‾", length)) +
@@ -125,11 +145,12 @@ Issue.prototype.getMessage = function(colorize)
                  chalk[colorMap.caret]("^") +
                  trimRight(highlights.substring(this.lineInfo.column + 1));
 
-    var message = messageTemplate(
+    var message = this.message.charAt(0).toLowerCase() + this.message.substr(1),
+        formattedMessage = messageTemplate(
             {
                 context: coloredContext,
                 severity: severity,
-                message: chalk[colorMap.message](this.message),
+                message: chalk[colorMap.message](message),
                 source: this.source,
                 highlights: highlights
             }
@@ -137,68 +158,121 @@ Issue.prototype.getMessage = function(colorize)
 
     chalk.enabled = enabled;
 
-    return message;
+    return formattedMessage;
 };
 
-Issue.prototype.addHighlight = function(node)
+Issue.prototype.getStackTrace = function()
 {
-    this.highlightedNodes.push(node);
+    function finder(match)
+    {
+        var regex = new RegExp("^\\s+at " + match);
+
+        return function(element, index)
+        {
+            if (regex.test(element))
+            {
+                i = index;
+
+                return true;
+            }
+        };
+    }
+
+    var stack = this.stack.split("\n"),
+        i = 0,
+        first = 1; // index of the first item we want to keep
+
+    /*
+        Get rid of our internal stuff. Look for the following stack items:
+
+        - Object.exports.addAcornError
+        - 2 after addIssueFromArgs
+        - exports.addIssue
+        - new exports.*
+    */
+    if (stack.some(finder("Object\\.exports\\.addAcornError")))
+        first = i + 1;
+    else if (stack.some(finder("addIssueFromArgs")))
+        first = i + 2;
+    else if (stack.some(finder("exports\\.addIssue")) || stack.some(finder("new exports\\.")))
+        first = i + 1;
+
+    stack.splice(1, first - 1);
+
+    return stack.join("\n");
 };
 
-exports.Note = function(source, sourcePath, node, message)
+Issue.prototype.addHighlight = function(location)
 {
-    exports.Note.super_.call(this, source, sourcePath, node, message, "note");
+    this.highlightedNodes.push(location);
+};
+
+exports.Note = function(source, sourcePath, location, message)
+{
+    exports.Note.super_.call(this, source, sourcePath, location, message, "note");
     this.name = "Note";
 };
 
 util.inherits(exports.Note, Issue);
 
-exports.Warning = function(source, sourcePath, node, message)
+exports.Warning = function(source, sourcePath, location, message)
 {
-    exports.Warning.super_.call(this, source, sourcePath, node, message, "warning");
+    exports.Warning.super_.call(this, source, sourcePath, location, message, "warning");
     this.name = "Warning";
 };
 
 util.inherits(exports.Warning, Issue);
 
-exports.Error = function(source, sourcePath, node, message)
+exports.Error = function(source, sourcePath, location, message)
 {
-    exports.Error.super_.call(this, source, sourcePath, node, message, "error");
+    exports.Error.super_.call(this, source, sourcePath, location, message, "error");
     this.name = "Error";
 };
 
 util.inherits(exports.Error, Issue);
 
-exports.FatalError = function(source, sourcePath, node, message)
+exports.FatalError = function(source, sourcePath, location, message)
 {
-    exports.FatalError.super_.call(this, source, sourcePath, node, message || "a fatal error occurred", "error");
+    exports.FatalError.super_.call(this, source, sourcePath, location, message || "a fatal error occurred", "error");
     this.name = "FatalError";
 };
 
 util.inherits(exports.FatalError, exports.Error);
 
-exports.InternalError = function(source, sourcePath, node, message)
+exports.InternalError = function(source, sourcePath, location, message)
 {
-    exports.InternalError.super_.call(this, source, sourcePath, node, message || "an internal error occurred", "error");
+    exports.InternalError.super_.call(
+        this, source, sourcePath, location, message || "an internal error occurred", "error");
+
     this.name = "InternalError";
 };
 
 util.inherits(exports.InternalError, exports.FatalError);
 
-exports.addIssue = function(issues, Class, source, sourcePath, node, args)
+function slice(args, start)
 {
-    // If addIssue is called directly, args is the message, optionally
-    // followed by format args. Otherwise it was called by addNote etc.
-    // and args is the arguments passed to those methods.
-    if (typeof args === "string")
-        args = Array.prototype.slice.call(arguments, 5);
-    else
-        args = Array.prototype.slice.call(args, 4);
+    var copy = [];
+
+    if (start === undefined)
+        start = 0;
+
+    for (var i = start; i < args.length; ++i)
+        copy.push(args[i]);
+
+    return copy;
+}
+
+exports.addIssue = function(Class, issues, source, sourcePath, location, message)
+{
+    // Mozilla docs say not to use Array.prototype.slice on arguments
+    var args = slice(arguments, 6);
+
+    args.unshift(message);
 
     var issue = new Class(
             source,
             sourcePath,
-            node,
+            location,
             util.format.apply(null, args)
         );
 
@@ -207,44 +281,55 @@ exports.addIssue = function(issues, Class, source, sourcePath, node, args)
     return issue;
 };
 
-exports.addNote = function(issues, source, sourcePath, node)
+function addIssueFromArgs(Class, args)
 {
-    return exports.addIssue(issues, exports.Note, source, sourcePath, node, arguments);
+    var newArgs = slice(args);
+
+    newArgs.unshift(Class);
+
+    return exports.addIssue.apply(null, newArgs);
+}
+
+/* eslint-disable no-unused-vars */
+
+exports.addNote = function(issues, source, sourcePath, location, message)
+{
+    return addIssueFromArgs(exports.Note, arguments);
 };
 
-exports.addWarning = function(issues, source, sourcePath, node)
+exports.addWarning = function(issues, source, sourcePath, location, message)
 {
-    return exports.addIssue(issues, exports.Warning, source, sourcePath, node, arguments);
+    return addIssueFromArgs(exports.Warning, arguments);
 };
 
-exports.addError = function(issues, source, sourcePath, node)
+exports.addError = function(issues, source, sourcePath, location, message)
 {
-    return exports.addIssue(issues, exports.Error, source, sourcePath, node, arguments);
+    return addIssueFromArgs(exports.Error, arguments);
 };
 
-exports.addFatalError = function(issues, source, sourcePath, node)
+exports.addFatalError = function(issues, source, sourcePath, location, message)
 {
-    exports.addIssue(issues, exports.FatalError, source, sourcePath, node, arguments);
-    throw new exports.FatalError();
+    return addIssueFromArgs(exports.FatalError, arguments);
 };
 
-exports.addInternalError = function(issues, source, sourcePath, node)
+exports.addInternalError = function(issues, source, sourcePath, location, message)
 {
-    exports.addIssue(issues, exports.InternalError, source, sourcePath, node, arguments);
-    throw new exports.InternalError();
+    return addIssueFromArgs(exports.InternalError, arguments);
 };
+
+/* eslint-enable */
 
 var stripLocRE = /^(.+)\s*\(\d+:\d+\)$/;
 
 exports.addAcornError = function(issues, error, source, sourcePath)
 {
-    // Make a fake node object that contains the start position of the error
-    var node = {
+    // Make a fake location object that contains the start position of the error
+    var location = {
             start: error.pos,
             end: error.pos,
             loc: { line: error.loc.line, column: error.loc.column }
         },
-        message = error.message.charAt(0).toLowerCase() + error.message.substr(1),
+        message = error.message,
 
         // Strip (line:column) from message
         match = stripLocRE.exec(message);
@@ -256,7 +341,7 @@ exports.addAcornError = function(issues, error, source, sourcePath)
         issues,
         source,
         sourcePath,
-        node,
+        location,
         message
     );
 };
@@ -275,11 +360,20 @@ exports.getWarningCount = function(issues)
     return errors.length;
 };
 
-exports.logIssues = function(issues, colorize)
+exports.getFormattedIssues = function(issues, colorize)
 {
     colorize = arguments.length > 1 ? !!colorize : true;
 
     var reporter = new reporters.StandardReporter(colorize);
+
+    return reporter.report(issues);
+};
+
+exports.logIssues = function(issues, colorize)
+{
+    colorize = arguments.length > 1 ? !!colorize : true;
+
+    var reporter = new reporters.ConsoleReporter(colorize);
 
     reporter.report(issues);
 };
